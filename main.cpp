4e95bf3f97543db8aca4103b7502d01e2e9b26f6
CarlM-69 <carlmathewgabay123@gmail.com>
2025-11-12 23:36:15 +0800

    Implement attendance check functionality and remove outdated attendance log

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+#define MAX_STUDENTS 100
+
+string studentNames[MAX_STUDENTS];
+int studentCount = 0;
+
+void refreshStudentList() {
+	string lineBuffer;
+	ifstream studentFile("students.txt");
+
+	if(!studentFile) {
+		cout << "ERROR: Can't find students text file!" << endl;
+		return;
+	}
+
+	for(studentCount = 0; studentCount < MAX_STUDENTS && getline(studentFile, lineBuffer); studentCount++) {
+		studentNames[studentCount] = lineBuffer;
+	}
+
+	studentFile.close();
+}
+
+void AttendanceCheck() {
+	string studentStatus[MAX_STUDENTS];
+
+	if(studentCount == 0) {
+		cout << ">> NO STUDENTS TO CHECK FOR ATTENDANCE!" << "\n\n";
+		return;
+	}
+
+	for(int i = 0; i < studentCount; i++) {
+		while(1) {
+			char status;
+
+			cout << i+1 << ". " << studentNames[i] << endl;
+			cout << "\t+ A: Absent" << endl;
+			cout << "\t+ B: Present" << endl;
+			cout << "\t+ C: Late" << endl;
+			cout << "\t>> ";
+			cin >> status;
+			status = tolower(status);
+
+			switch(status) {
+				case 'a': { studentStatus[i] = "Absent"; break; }
+				case 'b': { studentStatus[i] = "Present"; break; }
+				case 'c': { studentStatus[i] = "Late"; break; }
+				default: { cout << ">> INVALID!" << endl; continue; }
+			}
+
+			cout << studentNames[i] << " is " << studentStatus[i] << endl;
+			break;
+		}
+	}
+
+	time_t currentTime = time(0);
+	tm* localTime = localtime(&currentTime);
+
+	string filename = "AttendanceLogs/attendance_"
+		+ to_string()
+}
+
+int main() {
+	refreshStudentList();
+
+	while(1) {
+		char choice;
+
+		cout << endl << "\t\tTrackMate" << "\n\n";
+		
+		cout << "+ A: Start Attendance Check" << endl;
+		cout << "+ B: Modify Students" << endl;
+		cout << "+ C: Exit App" << endl;
+		cout << ">> ";
+		cin >> choice;
+		choice = tolower(choice);
+
+		if(choice == 'a') {
+			cout << ">> STARTING ATTENDANCE CHECK" << endl;
+			// AttendanceCheck();
+		} else if(choice == 'b') {
+			cout << ">> MODIFYING STUDENTS" << endl;
+			// ModifyStudents();
+		} else if(choice == 'c') {
+			cout << ">> EXITING APP" << endl;
+			break;
+		} else {
+			cout << ">> INVALID!" << endl;
+			continue;
+		}
+	}
+	return 0;
+}
